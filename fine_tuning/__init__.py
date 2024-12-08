9d23699acd348e3e5e2b2622dc286fc59b57c947
kevin-le1 <kl3059@princeton.edu>
2024-12-08 15:14:35 -0500

    starting code

---
+++ fine_tuning/__init__.py
@@ -0,0 +1,12 @@
+# Instantiate an LLM instance & initialize it for future use.
+
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+# Set the OpenAI API key environment variable
+os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+
+llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini", max_tokens=2000)
